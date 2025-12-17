6a8e9d2fdef8cfddd31938d79b1835b7a4efd952
mohamed-rafat2001 <mohamed20rafat@gmail.com>
2025-12-17 19:35:58 +0200

    refactor(utils): remove duplicate object filter function
    The objectFilter.js file was deleted as its functionality was redundant with validationBody.js. The commented code in validationBody.js was also cleaned up since it was no longer needed.

--- server/utils/objectFilter.js
+++
@@ -1,8 +0,0 @@
-const objectFilter = (obj, objectFields) => {
-	const newObject = {};
-	Object.keys(obj).forEach((el) => {
-		if (objectFields.includes(el)) newObject[el] = req.body[el];
-	});
-	return newObject;
-};
-export default objectFilter;
--- server/utils/validationBody.js
+++ server/utils/validationBody.js
@@ -7,15 +7,3 @@ const validationBody = (obj, allowedFields) => {
 	return newObject;
 };
 export default validationBody;
-
-// const validationBody = (obj, objectFields) => {
-// 	let newObject = {};
-// 	// Loop through the object fields
-// 	for (const field of objectFields) {
-// 		if (field in obj) {
-// 			// Check if property exists (not if it's truthy)
-// 			newObject[field] = obj[field];
-// 		} else {
-// 			return {};
-// 		}
-// 	}
